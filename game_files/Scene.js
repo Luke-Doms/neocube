b30568a1e2b57f0ad5a4da0b88cf0932b42f96da
Luke <90083045+Luke-Doms@users.noreply.github.com>
2024-09-06 20:21:03 -0400

    shader loaders and sketch of render pipeline added

--- game_files/Scene.js
+++ game_files/Scene.js
@@ -1,4 +1,5 @@
 import {CreateModel} from './lib/utils/CreateModel.js';
+import {GetShaderText, CreateProgram} from './lib/utils/CreateProgram';
 
 export class Scene {
   constructor (gl, dimensions) {
@@ -6,16 +7,54 @@ export class Scene {
     this.dims = dimensions;
   }
 
-  Load() {
+  async Load() {
+    var self = this;
+    this.program;
+    this.buffer;
+    this.viewMatrix;
+    this.projMatrix;
+    this.look;
+
     const triangle = new Float32Array([1, 0, 0, 1, 0, 0, 
                                        0, 1, 0, 0, 1, 0, 
                                        0, 0, 1, 0, 0, 1]);
+
+    const vertexShaderText = await GetShaderText('game_files/lib/shaders/VertexShader.glsl');
+    const fragmentShaderText = await GetShaderText('game_files/lib/shader/FragmentShader.glsl');
+
+    const this.program = CreateProgram(this.gl, vertexShaderText, fragmentShaderText);
+
+    const this.buffer = CreateModel(this.gl, triangle);
+    me.program.uniforms = {
+      u_Proj: me.gl.getUniformLocation(me.program, 'u_Proj'),
+      u_View: me.gl.getUniformLocation(me.program, 'u_View'),
+      u_World: me.gl.getUniformLocation(me.program, 'u_World'),
+    };
+
+    me.program.attribs = {
+      a_Position: me.gl.getAttribLocation(me.Program, 'a_Position'),
+      a_Color: me.gl.getAttribLocation(me.Program, 'a_Color'),
+    };
   }
 
   Unload() {
   }
 
   Begin() {
+    var gl = this.gl;
+    
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearColor(0.08, 0.12, 0.18, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(self.program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.enableVertexAttribArray(this.program.attribs.a_Position);
+    gl.vertexAttribPointer(this.program.attributeLocation, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(this.program.attribs.a_Color);
+    gl.vertexAttribPointer(this.program.attributeLocation, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
   }
 
   Update() {
--- game_files/lib/utils/CreateProgram.js
+++ game_files/lib/utils/CreateProgram.js
@@ -1,5 +1,36 @@
 
+aysnc function GetShaderText(shader) {	
+	const response = await fetch(shader);
+	const shaderText = await response.text();
+	return shaderText;
+}
 
-export function CreateShaderProgram(gl) {
-	return null;
+export function CreateShaderProgram(gl, vsText, fsText) {
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(vertexShader, vsText);
+	gl.shaderSource(fragmentShader, fsText);
+
+	gl.compileShader(vertexShader);
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		throw `failed to compile vertex shader ${gl.getShaderInfoLog(vertexShader)}`;
+	}
+	gl.compileShader(fragmentShader):
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		throw `failed to compile fragment shader ${gl.getShaderInfoLog(fragmentShader)}`;
+	}
+
+	const program = gl.createProgram();
+
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+
+	gl.linkProgram(program);
+
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		const info = gl.getProgramInfoLog(program);
+		throw `Failed to compile program \n ${info}`;
+	}
+
+	return program;
 }
