d71192e4debc81733c7a3d7a15c25d3feeed918e
Luke <90083045+Luke-Doms@users.noreply.github.com>
2024-09-27 23:29:59 -0400

    removed server file

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-const PORT = 3003;
-
-app.use(express.static(__dirname));
-app.listen(PORT, () => {
-	console.log(`Port started at ${PORT}`);
-	});
